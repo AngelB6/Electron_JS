8ea474567158a232442a6c286f9cca1a427f3a8a
AngelB6 <rodrigobarreraangelromero@gmail.com>
2024-05-08 17:52:28 -0500

    Estilos con tailwindCSS. Continua el fallo en la conexion a la BD

--- CRUD-GPT/src/database.js
+++ CRUD-GPT/src/database.js
@@ -1,4 +1,4 @@
-const mysql = require("mysql2/promise");
+const mysql = require('mysql2/promise');
 
 let connection;
 
@@ -6,20 +6,20 @@ async function conectarBD() {
   if (!connection) {
     try{ 
     connection = await mysql.createConnection({
-      host: "localhost",
-      user: "root",
-      password: "",
-      database: "test"
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'test'
     });
-    console.log("Conectado a la BD");
-    console.log("Estado de la conexion:", connection.state);
+    console.log('Conectado a la BD');
+    console.log('Estado de la conexion:', connection.state);
     }
     catch (error){
-      console.error("Error de conexion con la BD: ", error)
+      console.error('Error de conexion con la BD: ', error)
       connection = null
     }
   } else {
-    console.log("Ya existe una conexion a la BD");
+    console.log('Ya existe una conexion a la BD');
   }
 }
 
