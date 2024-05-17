770b68509a023c1d23ec090d2e4d507e52a24887
AngelB6 <rodrigobarreraangelromero@gmail.com>
2024-05-17 12:15:06 -0500

    Proyecto Movido
    Proyecto movido a un nuevo repositorio

--- CRUD-GPT/src/apps/appMain.js
+++
@@ -1,7 +0,0 @@
-const buttFormulario = document.getElementById("btnFormulario");
-
-const { ipcRenderer } = require("electron");
-
-buttFormulario.addEventListener("click", () => {
-  ipcRenderer.send("cambiarPantalla", "../view/formulario.html");
-});
